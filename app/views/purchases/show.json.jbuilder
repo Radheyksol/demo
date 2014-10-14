9fdacedf3d604b9bdc5b678d89d319bf120a5bb4
om <om.prakash@ksolves.com>
2014-10-14 11:08:43 +0530

    second_commit

---
+++ app/views/purchases/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @purchase, :id, :price, :created_at, :updated_at
---
+++ db/migrate/20141013115036_create_purchases.rb
@@ -0,0 +1,9 @@
+class CreatePurchases < ActiveRecord::Migration
+  def change
+    create_table :purchases do |t|
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
